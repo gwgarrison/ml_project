3f97c62c27a819f54d2894ba3a4f70be85ebfe67
gooblagoo <garrisongw@gmail.com>
2014-08-19 13:46:11 -0600

    8/19/2014 commit

--- raw_script/PA.R
+++ raw_script/PA.R
@@ -29,6 +29,7 @@ pml <- pml[,-c(1:6)]
 pml$classe <- factor(pml$classe)
 
 # split data into training and test sets
+set.seed(77)
 inTrain <- createDataPartition(y=pml$classe,p=0.6, list=FALSE)
 training <- pml[inTrain,]
 testing <- pml[-inTrain,]
@@ -45,15 +46,32 @@ testing <- pml[-inTrain,]
 #rFit <- train(classe~ .,data=training,method="rpart")
 #gbmFit <- train(classe ~ ., method="gbm",data=training,verbose=FALSE,tuneLength=5)
 #rPred <- predict(rFit,newdata=testing)
+knn.fit <- train(classe ~.,data=training,method="knn")
+knnpred <- predict(knn.fit,newdata=testing)
+confusionMatrix(table(knnpred,testing$classe))
 
 library(randomForest)
 rf.fit <- randomForest(classe ~., data=training,type="class")
 rfpred <- predict(rf.fit,newdata=testing)
 rf.fit
-rf.table <- table(rfpred,testing$classe)
-confusionMatrix(rf.table)
+confusionMatrix(table(rf.fit$predicted,training$classe))
+confusionMatrix(table(rfpred,testing$classe))
+vi <- varImp(rf.fit)
 varImpPlot(rf.fit)
 
+# submission
+pml.test <- read.csv("./raw_data/pml-testing.csv",stringsAsFactors=FALSE)
+answers <- predict(rf.fit,newdata=pml.test)
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("./answers/problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+pml_write_files(answers)
 # References
 1. Velloso, E.; Bulling, A.; Gellersen, H.; Ugulino, W.; Fuks, H. Qualitative Activity Recognition of Weight Lifting Exercises. Proceedings of 4th International Conference in Cooperation with SIGCHI (Augmented Human '13) . Stuttgart, Germany: ACM SIGCHI, 2013.
 2. Kuhn, Max Building Predictive Models in R Using the caret Package. Journal of Statistical Software. Vol. 28. November 2008.
+3. Breiman, L; Cutler, A; Random Forests. http://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm
